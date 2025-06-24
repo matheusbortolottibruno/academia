c25ec4366c40454b6459f2a73a5fb04b26507768
matheusbortolottibruno <matheus_bortolotti@yahoo.com>
2025-06-23 22:26:31 -0300

    academia

---
+++ baner.php
@@ -0,0 +1,22 @@
+baner
+<div id="carouselExample" class="carousel slide">
+    <div class="carousel-inner">
+        <div class="carousel-item active">
+            <img src="img/banner.jpg" class="d-block w-100 banner" alt="...">
+        </div>
+        <div class="carousel-item">
+            <img src="img/banner.jpg" class="d-block w-100 banner" alt="...">
+        </div>
+        <div class="carousel-item">
+            <img src="img/banner.jpg" class="d-block w-100 banner" alt="...">
+        </div>
+    </div>
+    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Previous</span>
+    </button>
+    <button class="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Next</span>
+    </button>
+</div>
