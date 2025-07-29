5f3c5c53dd0b9556ec39de5fdd1575855ea4f863
matheusbortolottibruno <110576109+matheusbortolottibruno@users.noreply.github.com>
2025-07-28 21:41:52 -0300

    sac

---
+++ clienteformulario.php
@@ -0,0 +1,11 @@
+<?php
+//formulario de cadastro
+?>
+
+<h1>Cadastro</h1>
+<form name="cadastro" method= 'post' action="clientesalvar.php">
+nome: <input type="text" name="nome" required><br>
+email: <input type="text" name="email" required><br>
+<button type="submit">Cadastrar cliente</button>
+
+</form>
